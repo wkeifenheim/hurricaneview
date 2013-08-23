91f63d4811c85dcdfe8db9a3226a8688bb168553
Woodrow Keifenheim <woodrowk@gmail.com>
2013-08-23 12:47:33 -0500

    basic functions pass minimum criteria. updated to section of worldmap rather than global map

--- hv_controls.m
+++ hv_controls.m
@@ -46,88 +46,91 @@ end
 
 % --- Executes just before hv_controls is made visible.
 function hv_controls_OpeningFcn(hObject, eventdata, handles, varargin)
-% This function has no output args, see OutputFcn.
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to hv_controls (see VARARGIN)
+    % This function has no output args, see OutputFcn.
+    % hObject    handle to figure
+    % eventdata  reserved - to be defined in a future version of MATLAB
+    % handles    structure with handles and user data (see GUIDATA)
+    % varargin   command line arguments to hv_controls (see VARARGIN)
 
 
-s = load('HurDat_1851_2010.mat');
-handles.hurDat = s.hurDat;
-handles.points = zeros(1,41198);   % store handles to plotted points
-                                  
+    s = load('HurDat_1851_2010.mat');
+    handles.hurDat = s.hurDat;
+    handles.points = zeros(1,41198);   % store handles to plotted points
 
-% Create a 1442x2 matrix containing the indeces of a specific hurricane
-% in the hurDat matrix -> HurricaneIndex(somehurricane#) =
-% [startindex,endindex]
 
-handles.HurricaneIndex = zeros([1442,2]);
-
-handles.HurricaneIndex(1,1) = 1;
-for i=1:41197
-    j = handles.hurDat(i,1);
-    if(handles.hurDat(i+1,1) ~= j)
-        handles.HurricaneIndex(j,2) = i;
-        handles.HurricaneIndex(j+1,1) = i+1;
+    % Create a 1442x3 matrix containing the indeces of a specific hurricane
+    % in the hurDat matrix -> HurricaneIndex(somehurricane#) =
+    % [startindex,endindex,plotted]
+    handles.HurricaneIndex = zeros([1442,3]);
+    handles.HurricaneIndex(1,1) = 1;
+    for i=1:41197
+        j = handles.hurDat(i,1);
+        if(handles.hurDat(i+1,1) ~= j)
+            handles.HurricaneIndex(j,2) = i;
+            handles.HurricaneIndex(j+1,1) = i+1;
+        end
     end
-end
-handles.HurricaneIndex(1442,2) = 41198;
+    handles.HurricaneIndex(1442,2) = 41198;
 
-% Maintain a history of which hurricanes have been plotted so we can
-% easily clear the most recent
+    % Maintain a history of which hurricanes have been plotted so we can
+    % easily clear the most recent
+    handles.HurIndexHist = 0;
 
-handles.HurIndexHist = 0;
+    % Handle for keeping track of plotting hurricanes stepwise
+    handles.stepPlace = 0;
+    
+    % Used so attempting to step repeatedly on the last hurricane
+    % coordinate doesn't overwrite the cooresponding original handle
+    handles.plotStop = 0;
 
-% Handle for keeping track of plotting hurricanes stepwise
-handles.stepPlace = 0;
+    % Load up the map
+    worldmap([0 70],[-120,0])
+    load coast
+    plotm(lat,long)
+    %axesm mollweid
+    %framem('FEdgeColor','blue','FLineWidth',0.5)
+    %plotm(lat,long,'LineWidth',1,'Color','blue')
 
-% Load up the map
-load coast
-axesm mollweid
-framem('FEdgeColor','blue','FLineWidth',0.5)
-plotm(lat,long,'LineWidth',1,'Color','blue')
+    % Choose default command line output for hv_controls
+    handles.output = hObject;
 
-% Choose default command line output for hv_controls
-handles.output = hObject;
+    % Update handles structure
+    guidata(hObject, handles);
 
-% Update handles structure
-guidata(hObject, handles);
-
-% UIWAIT makes hv_controls wait for user response (see UIRESUME)
-% uiwait(handles.figure1);
+    % UIWAIT makes hv_controls wait for user response (see UIRESUME)
+    % uiwait(handles.figure1);
 end
 
 % --- Outputs from this function are returned to the command line.
 function varargout = hv_controls_OutputFcn(hObject, eventdata, handles) 
-% varargout  cell array for returning output args (see VARARGOUT);
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
+    % varargout  cell array for returning output args (see VARARGOUT);
+    % hObject    handle to figure
+    % eventdata  reserved - to be defined in a future version of MATLAB
+    % handles    structure with handles and user data (see GUIDATA)
 
-% Get default command line output from handles structure
-varargout{1} = handles.output;
+    % Get default command line output from handles structure
+    varargout{1} = handles.output;
 
 end
 
+% --- Updates the choice of hurricane input by the user
 function hurChoice_Callback(hObject, eventdata, handles)
-% hObject    handle to hurChoice (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
+    % hObject    handle to hurChoice (see GCBO)
+    % eventdata  reserved - to be defined in a future version of MATLAB
+    % handles    structure with handles and user data (see GUIDATA)
 
-% Hints: get(hObject,'String') returns contents of hurChoice as text
-%        str2double(get(hObject,'String')) returns contents of hurChoice as a double
+    % Hints: get(hObject,'String') returns contents of hurChoice as text
+    %        str2double(get(hObject,'String')) returns contents of hurChoice as a double
 
-user_entry = str2double(get(hObject,'string'));
-if isnan(user_entry)
-    errordlg('You must enter a numeric value','Bad Input','modal')
-    uicontrol(hObject)
-        return
+    user_entry = str2double(get(hObject,'string'));
+    if isnan(user_entry)
+        errordlg('You must enter a numeric value','Bad Input','modal')
+        uicontrol(hObject)
+            return
+    end
+    handles.choice = user_entry;
+    guidata(hObject,handles);
 end
-handles.choice = user_entry;
-guidata(hObject,handles);
-end
-
 
 % --- Executes during object creation, after setting all properties.
 function hurChoice_CreateFcn(hObject, eventdata, handles)
@@ -148,12 +151,12 @@ function clearMap_Callback(hObject, eventdata, handles)
     % Establish which was the last hurricane to be plotted
     n = size(handles.HurIndexHist,2);
         % debugging stuff
-        display('Beginning clearMap function...')
-        disp(strcat('Current HurIndexHist: ',num2str(handles.HurIndexHist)))
-        disp(strcat('N: ',num2str(n)))
+        %display('Beginning clearMap function...')
+        %disp(strcat('Current HurIndexHist: ',num2str(handles.HurIndexHist)))
+        %disp(strcat('N: ',num2str(n)))
 
     if(handles.stepPlace ~= 0) % "step back"
-        disp(strcat('attempting backstep from stepPlace: ',num2str(handles.stepPlace)))
+        %disp(strcat('attempting backstep from stepPlace: ',num2str(handles.stepPlace)))
 
         try
             delete(handles.points(handles.stepPlace));
@@ -164,9 +167,9 @@ function clearMap_Callback(hObject, eventdata, handles)
         handles.stepPlace = handles.stepPlace - 1;
 
         % Reset stepPlace if decremented past first index
-            disp(strcat('first index of current hurricane: ',...
-                num2str(handles.HurricaneIndex(handles.HurIndexHist(n),1))));
-            disp(strcat('stepPlace:',num2str(handles.stepPlace),' < ',num2str(handles.HurricaneIndex(handles.HurIndexHist(n),1))))
+            %disp(strcat('first index of current hurricane: ',...
+            %    num2str(handles.HurricaneIndex(handles.HurIndexHist(n),1))));
+            %disp(strcat('stepPlace:',num2str(handles.stepPlace),' < ',num2str(handles.HurricaneIndex(handles.HurIndexHist(n),1))))
         if(handles.stepPlace < handles.HurricaneIndex(handles.HurIndexHist(n),1))
             handles.stepPlace = 0;
             % And pop this hurricane off the history stack
@@ -188,6 +191,9 @@ function clearMap_Callback(hObject, eventdata, handles)
             disp('caught it, we`re good')
         end
 
+        % Mark the hurricane as not plotted
+        handles.HurricaneIndex(handles.HurIndexHist(n),3) = 0;
+        
         % "Pop" the most recent value from the history
         if(n == 1) % Last remaining plotted hurricane
             handles.HurIndexHist = 0;
@@ -205,70 +211,25 @@ end
 % --- Executes on button press in stepPath.
 function stepPath_Callback(hObject, eventdata, handles)
 
-% If the step tracker is outside the range of indices for the current
-% hurricane, set the tracker to the first index for the hurricane
-if(handles.stepPlace < handles.HurricaneIndex(handles.choice,1)...
-        || handles.stepPlace > handles.HurricaneIndex(handles.choice,2))
-    
-    handles.stepPlace = handles.HurricaneIndex(handles.choice,1);
-    
-    %This is the first instance of plotting this
-    %hurricane, so append it to the plot history
-    if(handles.HurIndexHist == 0) %initial case
-        handles.HurIndexHist = handles.choice;
-    else
-        handles.HurIndexHist = [handles.HurIndexHist,handles.choice];
+    % If the step tracker is outside the range of indices for the current
+    % hurricane, set the tracker to the first index for the hurricane
+    if(handles.stepPlace < handles.HurricaneIndex(handles.choice,1)...
+            || handles.stepPlace > handles.HurricaneIndex(handles.choice,2))
+
+        handles.stepPlace = handles.HurricaneIndex(handles.choice,1);
+        handles.plotStop = 0;
+
+        %If first instance of plotting this
+        %hurricane, append it to the plot history
+        if(handles.HurIndexHist == 0) %initial case
+            handles.HurIndexHist = handles.choice;
+        else
+            handles.HurIndexHist = [handles.HurIndexHist,handles.choice];
+        end
     end
-end
 
-%Determine appropriate hurricane category color
-windspeed = handles.hurDat(handles.stepPlace,10);
-if(windspeed <= 95)
-    linespec = 'g*'; %category 1
-elseif(windspeed <= 110)
-    linespec = 'y*'; %category 2
-elseif(windspeed <= 129)
-    linespec = 'c*'; %category 3
-elseif(windspeed <= 156)
-    linespec = 'r*'; %category 4
-else
-    linespec = 'k*'; %category 5
-end
-
-% Plot the step, and increment the step tracker
-%disp(strcat('plotting point cooresponding to stepPlace:',num2str(handles.stepPlace)))
-handles.points(handles.stepPlace) = plotm(handles.hurDat(handles.stepPlace,6),...
-    handles.hurDat(handles.stepPlace,7),linespec);
-if(handles.stepPlace ~= handles.HurricaneIndex(handles.choice,2))
-    handles.stepPlace = handles.stepPlace + 1;
-end
-
-guidata(hObject,handles);
-
-end
-
-% --- Executes on button press in plotPath.
-function plotPath_Callback(hObject, eventdata, handles)
-% hObject    handle to plotPath (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-j = handles.HurricaneIndex(handles.choice,1);
-k = handles.HurricaneIndex(handles.choice,2);
-
-% Append this hurricane to the history "stack"...
-if(handles.HurIndexHist == 0) %initial case
-    handles.HurIndexHist = handles.choice;
-else
-    handles.HurIndexHist = [handles.HurIndexHist,handles.choice];
-end
-
-
-% Iterate over the indeces of hurDat cooresponding to the choice of
-% Hurricane#
-for i=j:k
     %Determine appropriate hurricane category color
-    windspeed = handles.hurDat(i,10);
+    windspeed = handles.hurDat(handles.stepPlace,10);
     if(windspeed <= 95)
         linespec = 'g*'; %category 1
     elseif(windspeed <= 110)
@@ -280,10 +241,66 @@ for i=j:k
     else
         linespec = 'k*'; %category 5
     end
-    
-    % Aaaaand plot the coordinate
-    handles.points(i) = plotm(handles.hurDat(i,6),handles.hurDat(i,7),linespec);
-    
+
+    % Plot the step, and increment the step tracker
+    %disp(strcat('plotting point cooresponding to stepPlace:',num2str(handles.stepPlace)))
+    if(handles.plotStop == 0)
+        handles.points(handles.stepPlace) = plotm(handles.hurDat(handles.stepPlace,6),...
+            handles.hurDat(handles.stepPlace,7),linespec,'MarkerSize',8);
+    end
+    if(handles.stepPlace ~= handles.HurricaneIndex(handles.choice,2))
+        handles.stepPlace = handles.stepPlace + 1;
+    else
+        disp('Current Hurricane is already fully plotted. Simmer down nah.')
+        handles.plotStop = 1; %Step will not plot the last coordinate again
+                              %in order to not lose the handle
+    end
+
+    guidata(hObject,handles);
+
 end
-guidata(hObject,handles)
+
+% --- Executes on button press in plotPath.
+function plotPath_Callback(hObject, eventdata, handles)
+
+    if(handles.HurricaneIndex(handles.choice,3) == 0)
+        j = handles.HurricaneIndex(handles.choice,1);
+        k = handles.HurricaneIndex(handles.choice,2);
+
+        % Append this hurricane to the history "stack"...
+        if(handles.HurIndexHist == 0) %initial case
+            handles.HurIndexHist = handles.choice;
+        else
+            handles.HurIndexHist = [handles.HurIndexHist,handles.choice];
+        end
+
+        % Iterate over the indeces of hurDat cooresponding to the choice of
+        % Hurricane#
+        for i=j:k
+            %Determine appropriate hurricane category color
+            windspeed = handles.hurDat(i,10);
+            if(windspeed <= 95)
+                linespec = 'g*'; %category 1
+            elseif(windspeed <= 110)
+                linespec = 'y*'; %category 2
+            elseif(windspeed <= 129)
+                linespec = 'c*'; %category 3
+            elseif(windspeed <= 156)
+                linespec = 'r*'; %category 4
+            else
+                linespec = 'k*'; %category 5
+            end
+
+            % Aaaaand plot the coordinate
+            handles.points(i) = plotm(handles.hurDat(i,6),handles.hurDat(i,7),...
+                linespec,'MarkerSize',8);
+
+        end
+        
+        % Mark the hurricane as plotted (true) in HurricaneIndex
+        handles.HurricaneIndex(handles.choice,3) = 1;
+        
+    end
+    
+    guidata(hObject,handles)
 end
